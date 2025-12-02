b26ae9d09cb943cebf43586018f6adcb290ea5dc
geltz <ethiquelia@gmail.com>
2025-12-02 16:58:44 -0500

    Update __init__.py

--- nodes/GaussianMask/__init__.py
+++ nodes/GaussianMask/__init__.py
@@ -10,8 +10,8 @@ class GaussianMask:
         return {
             "required": {
                 "conditioning": ("CONDITIONING",),
-                "randomize_percent": ("FLOAT", {"default": 50.0, "min": 0.0, "max": 100.0, "step": 1, "tooltip": "Percentage of embedding values to modify."}),
-                "strength": ("FLOAT", {"default": 20, "min": 0.0, "max": 1000.0, "step": 0.1, "tooltip": "Scale of the random noise."}),
+                "randomize_percent": ("FLOAT", {"default": 25.0, "min": 0.0, "max": 100.0, "step": 1, "tooltip": "Percentage of embedding values to modify."}),
+                "strength": ("FLOAT", {"default": 25, "min": 0.0, "max": 1000.0, "step": 0.1, "tooltip": "Scale of the random noise."}),
                 "noise_insert": (["noise on beginning steps", "noise on ending steps", "noise on all steps"],),
                 "steps_switchover_percent": ("FLOAT", {"default": 5.0, "min": 0.0, "max": 100.0, "step": 1, "tooltip": "Percentage of steps before switching conditioning."}),
                 "seed": ("INT", {"default": 1, "min": 0, "max": 0xFFFFFFFF, "step": 1})
